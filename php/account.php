4c0ebac92b02fd47ed70c52303af52704358256b
michael e rogowski wis student <mer4@afsconnect2.njit.edu>
2017-11-10 13:53:05 -0500

    account.php edit

--- php/account.php
+++ php/account.php
@@ -1,7 +1,8 @@
 <?php
 
-$hostname = ""     ; 			// or "sql2.njit.edu"   OR "SQL1.NJIT.EDU"
-$username = "" ;
-$project  = "" ;
-$password = "" ;
+$hostname = "sql2.njit.edu"; 			// or "sql2.njit.edu"   OR "SQL1.NJIT.EDU"
+$username = "mer4";
+$project  = "mer4";
+$password = "2kXQOxHZC";
+
 ?>
